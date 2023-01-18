66c5ec759c16636a6be2dcd47a38c0047ee9bcaf
NathanHuetsch <nathan.huetsch@gmail.com>
2023-01-18 09:48:34 +0100

    update

--- runfiles/warm_experiment.sh
+++ runfiles/warm_experiment.sh
@@ -1,5 +1,5 @@
-#PBS -q gshort
-#PBS -l nodes=1:ppn=1:gpus=1:gshort
+#PBS -q a30
+#PBS -l nodes=1:ppn=1:gpus=1:a30
 #PBS -l walltime=40:00:00
 #PBS -d /remote/gpu07/huetsch
 
@@ -14,5 +14,5 @@ module load cuda/11.7
 # cd into the project folder
 cd GenerativeJetting
 
-python run_Z2.py --warm_start_path="/remote/gpu07/huetsch/GenerativeJetting/runs/6d/TBD_Resnet_DenseNet512_long_5e52826"
+python run_Z2.py --warm_start_path="/remote/gpu07/huetsch/GenerativeJetting/runs/13d/TBD_DenseNet1024d_6l_CosSchedule_l2_linear6760"
 
