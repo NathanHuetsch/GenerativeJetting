41f36b325a845e43128445b2276971766ae11b6a
Jonas Spinner <jonasspinner@gmx.de>
2023-01-24 14:38:04 +0100

    Added extended sampling function for the autoregression money plot

--- Source/Models/autoregGMM.py
+++ Source/Models/autoregGMM.py
@@ -6,6 +6,7 @@ from Source.Models.ModelBase import GenerativeModel
 import torch
 import torch.nn.functional as F
 import torch.distributions as D
+import scipy.stats as stats
 
 class AutoRegGMM(GenerativeModel):
     """
@@ -24,6 +25,7 @@ class AutoRegGMM(GenerativeModel):
         assert intermediate_fac is not None, "build_model: intermediate_fac not specified"
         params["intermediate_dim"] = n_head * n_per_head
         n_gauss = get(params, "n_gauss", None)
+        self.n_gauss = n_gauss
         assert n_gauss is not None, "build_model: n_gauss not specified"
         print(f"Build model AutoRegGMM with n_head={n_head}, n_per_head={n_per_head}, n_blocks={n_blocks}, "
               f"intermediate_fac={intermediate_fac}, n_gauss={n_gauss}")
@@ -40,14 +42,14 @@ class AutoRegGMM(GenerativeModel):
         """Build the network"""
         return Source.Networks.attnetGMM(self.params).to(self.device)
     
-    def batch_loss(self, x, n_jets=2, conditional=False):
+    def batch_loss(self, x, conditional=False):
         """
         Loss function for autoregressive model
         TBD: Implement conditional
         :x: Training data in shape (batch_size, block_size)
         :returns: torch loss object
         """
-        x = torch.cat((n_jets*torch.ones(x.size(0), 1, device=self.device), x), dim=1)
+        x = torch.cat((self.n_jets*torch.ones(x.size(0), 1, device=self.device), x), dim=1)
         idx = x[:, :-1]
         targets = x[:, 1:]
 
@@ -59,12 +61,11 @@ class AutoRegGMM(GenerativeModel):
         
         return loss
     
-    def sample_n(self, n_samples, n_jets=2, conditional=False, prior_samples=None, con_depth=0):
+    def sample_n(self, n_samples, conditional=False, prior_samples=None, con_depth=0):
         """
         Event generation for autoregressive model
         TBD: Implement conditional
         :n_samples: Number of samples to be generated
-        :n_jets: Number of jets to be generated (only relevant when training on multiple multiplicities at the same time)
         :returns: Generated samples in the shape (n_samples, block_size)
         """
         self.eval()
@@ -74,7 +75,7 @@ class AutoRegGMM(GenerativeModel):
         for i in range(n_batches):
             t0=time.time()
 
-            idx = n_jets * torch.ones(self.batch_size, 1, dtype=torch.int, device=self.device).float()            
+            idx = self.n_jets * torch.ones(self.batch_size, 1, dtype=torch.int, device=self.device).float()            
             for iBlock in range(self.block_size):
                 mu, sigma, weights = self.net(idx)
                 
@@ -93,3 +94,46 @@ class AutoRegGMM(GenerativeModel):
                 
         sample = sample[:n_samples]
         return sample
+
+    def sample_n_bonus(self, n_samples, conditional=False, prior_samples=None, con_depth=0, prec=1000):
+        '''
+        Variant of sample_n that returns not only the samples, but also the generate Gaussian
+        mixture pdf (probstotal) and the pdfs of all individual gaussians (probsindiv)
+        '''
+        assert n_samples <= self.batch_size, "sample_n_bonus: Specified n_samples > batch_size. " \
+               "This is probably not intended, as this function should be used for visualization only."
+        
+        self.eval()
+
+        probstotal = np.zeros((n_samples, self.block_size, prec))                   # pdf of gaussian mixture
+        probsindiv = np.zeros((n_samples, self.block_size, self.n_gauss, prec))     # pdfs of individual gaussians
+        xs = np.zeros((self.block_size, prec))                                      # x-values of the pdfs
+        
+        idx = self.n_jets * torch.ones(self.batch_size, 1, dtype=torch.int, device=self.device).float()
+        for idim in range(self.block_size):
+            mu, sigma, weights = self.net(idx)
+
+            # generate distribution and next event (standard)                
+            mix = D.Categorical(weights[:,-1,:])
+            comp = D.Normal(mu[:,-1,:], sigma[:,-1,:])
+            gmm = D.MixtureSameFamily(mix, comp)
+            idx_next = gmm.sample_n(1).permute(1,0)
+            idx = torch.cat((idx, idx_next), dim=1)
+
+            # generate total pdf (using torch.distributions)
+            probs = torch.zeros(self.batch_size, prec, dtype=torch.float, device=self.device)
+            vals = torch.linspace(torch.min(idx_next), torch.max(idx_next), prec)
+            for ix in range(prec):
+                x=vals[ix]
+                probs[:,ix] = torch.exp(gmm.log_prob(x))
+            probstotal[:,idim,:] = probs.detach().cpu().numpy()[:n_samples,:]
+            xs[idim,:] = vals.detach().cpu().numpy()
+
+            # generate individual pdfs (by hand)
+            for igauss in range(self.n_gauss):
+                for isample in range(n_samples):
+                    probsindiv[isample,idim,igauss,:] = weights[isample,idim,igauss].detach().cpu().numpy() \
+                                * stats.norm.pdf(xs[idim,:], loc=mu[isample,idim,igauss].detach().cpu().numpy(),
+                                        scale=sigma[isample,idim,igauss].detach().cpu().numpy())
+        sample = idx.detach().cpu().numpy()[:n_samples,:]
+        return sample, xs, probstotal, probsindiv
