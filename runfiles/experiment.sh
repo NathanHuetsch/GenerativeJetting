2178920d06dd03be32aa448b2f5e6b8c2bb2429d
NathanHuetsch <nathan.huetsch@gmail.com>
2024-05-29 19:08:00 +0200

    param and runfile

---
+++ runfiles/experiment.sh
@@ -0,0 +1,15 @@
+#PBS -q a30
+#PBS -l nodes=1:ppn=1:gpus=1:a30
+#PBS -l walltime=40:00:00
+#PBS -d /remote/gpu07/huetsch
+#PBS -o output.txt
+#PBS -e error.txt
+
+export CUDA_VISIBLE_DEVICES=$(cat $PBS_GPUFILE | sed s/.*-gpu// )
+export PYTHONPATH=$PYTHONPATH:/remote/gpu07/huetsch/lib/python3.9/site-packages
+
+source venv/bin/activate
+cd GenerativeJetting
+
+python run_Zn.py $1
+
